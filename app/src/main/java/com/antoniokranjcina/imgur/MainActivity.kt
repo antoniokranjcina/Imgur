cc6a0bd354a6e4955c1ee0fa23bc5c8f123704ee
antonio <antonio.kranjcina@decode.agency>
2020-12-11 13:49:41 +0100

    Initial Commit

---
+++ app/src/main/java/com/antoniokranjcina/imgur/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.antoniokranjcina.imgur
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
